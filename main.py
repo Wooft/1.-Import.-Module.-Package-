fddaccdc8aaecdf6da7d3c07762e14cb1b19dfd4
Михаил Колесниченко <wooftyaa@gmail.com>
2022-09-13 22:52:02 +0300

    Homework Done

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-# This is a sample Python script.
+from datetime import date
+from application import salary
+from application.db import people as pee
+from quickstart import authorization
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def get_date():
+    cur_date = date.today()
+    return cur_date
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print(get_date())
+    salary.calculate_salary()
+    pee.get_employees()
+    authorization()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
